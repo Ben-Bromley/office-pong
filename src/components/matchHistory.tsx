7bcd68716f87d908acdbf15754d2af83a8f9a8b5
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-09-01 22:03:59 +0100

    sort matches

--- src/components/matchHistory.tsx
+++ src/components/matchHistory.tsx
@@ -9,22 +9,23 @@ type Props = {
 
 const GameHistory: React.FC<Props> = ({ players }) => {
   const matchesQuery = trpc.useQuery(["match.getAll"]);
-  let matches = matchesQuery;
+  let matches = matchesQuery.data;
 
-  const matchesToDisplay = matches?.data?.map(m => {
-    return (
-      <MatchHistoryItem key={m.id} game={m} players={players} />
-    )
+  // sort games by date
+  matches = matches?.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
+
+  const matchesToDisplay = matches?.map(m => {
+    return <MatchHistoryItem key={m.id} game={m} players={players} />
   })
 
   return (
     <section className="">
       <h2 className="text-lg font-bold mb-4">🏓 Match History</h2>
-        <ul className="max-h-80 overflow-scroll rounded-lg">
-          {!matches && <SkeletonLoader rows={3} />}
-          {matchesToDisplay?.length != 0 && matchesToDisplay}
-          {matchesToDisplay?.length == 0 && <p>Go Play Some Ping Pong!</p>}
-        </ul>
+      <ul className="max-h-80 overflow-scroll rounded-lg">
+        {!matches && <SkeletonLoader rows={3} />}
+        {matchesToDisplay?.length != 0 && matchesToDisplay}
+        {matchesToDisplay?.length == 0 && <p>Go Play Some Ping Pong!</p>}
+      </ul>
     </section>
   )
 }
