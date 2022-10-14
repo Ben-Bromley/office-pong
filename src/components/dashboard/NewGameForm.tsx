e281a3dccf16275b97c744f8fdaef896838cb2c5
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-10-14 21:29:24 +0100

    refactor arrow component

--- src/components/dashboard/NewGameForm.tsx
+++ src/components/dashboard/NewGameForm.tsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react';
 import { FC, useState } from 'react';
 import { trpc } from '../../utils/trpc';
 import { useQueryClient } from 'react-query';
+import RightPointingArrow from '../shared/RightPointingArrow';
 
 interface FieldProps {
   label: 'One' | 'Two';
@@ -156,19 +157,7 @@ const NewGameForm: FC = () => {
           className="text-white focus:outline-none font-normal rounded-sm text-sm px-4 py-2 text-center inline-flex items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black"
         >
           Submit Results
-          <svg
-            aria-hidden="true"
-            className="ml-2 -mr-1 w-3 h-3"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          <RightPointingArrow />
         </button>
       </div>
     </section>
